e4811ca4a5a90df581714d16e69ac7f1f753b206
Alex Lepauvre <alexander.lepauvre@ad.aesthetics.mpg.de>
2023-08-25 10:18:04 +0200

    Improve general plotting

--- eye_tracker/general_plotting.py
+++ eye_tracker/general_plotting.py
@@ -17,7 +17,7 @@ task = "prp"
 session = "1"
 data_type = "eyetrack"
 epoch_names = ["visual_onset"]
-crop = [-0.3, 3.0]
+crop = [-0.3, 3.5]
 picks = ["LPupil", "RPupil"]
 task_relevance = ["non-target", "irrelevant"]
 durations = ["0.5", "1", "1.5"]
@@ -36,7 +36,7 @@ def get_blink_onset(data, times):
     blinking_onsets = []
     data_onset = np.diff(data, axis=-1)
     for trial in range(data.shape[0]):
-           blinking_onsets.append(times[np.where(data_onset[trial, :] == 1)[0]])
+        blinking_onsets.append(times[np.where(data_onset[trial, :] == 1)[0]])
     return blinking_onsets
 
 
@@ -98,29 +98,20 @@ def plot_eyetracker_data(subject):
 
         # ================================================================
         # Plot the pupillometry:
-        fig, ax = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True)
+        fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, sharey=True)
         # Extract the left and right eye:
         pupil_epo = epochs.copy().pick(["LPupil", "RPupil"])
         for ind, lock in enumerate(locks):
             for soa in soas:
-                data_l = pupil_epo.copy()["/".join([lock, soa])].get_data(picks=["LPupil"])
-                data_r = pupil_epo.copy()["/".join([lock, soa])].get_data(picks=["RPupil"])
-                avg_l = np.mean(np.squeeze(data_l), axis=0)
-                avg_r = np.mean(np.squeeze(data_r), axis=0)
-                err_l = sem(np.squeeze(data_l), axis=0)
-                err_r = sem(np.squeeze(data_r), axis=0)
+                pupil_data = np.mean(pupil_epo.copy()["/".join([lock, soa])].get_data(), axis=1)
+                avg = np.mean(np.squeeze(pupil_data), axis=0)
+                err = sem(np.squeeze(pupil_data), axis=0)
                 # Plot the average pupil size in that condition:
-                ax[ind, 0].plot(epochs.times, avg_l, label=soa)
+                ax[ind].plot(epochs.times, avg, label=soa)
                 # Add the error around that:
-                ax[ind, 0].fill_between(epochs.times, avg_l - err_l, avg_l + err_l,
-                                        alpha=0.3)
-                # Plot the average pupil size in that condition:
-                ax[ind, 1].plot(epochs.times, avg_r, label=soa)
-                # Add the error around that:
-                ax[ind, 1].fill_between(epochs.times, avg_r - err_r, avg_r + err_r,
-                                        alpha=0.3)
-            ax[ind, 0].set_title("Lock: {}, Eye: {}".format(lock, "Left"))
-            ax[ind, 1].set_title("Lock: {}, Eye: {}".format(lock, "Right"))
+                ax[ind].fill_between(epochs.times, avg - err, avg + err,
+                                     alpha=0.3)
+            ax[ind].set_title("Lock: {}".format(lock))
         plt.legend()
         plt.ylabel("Pupil size (norm.)")
         plt.xlabel("Time (s)")
@@ -132,41 +123,35 @@ def plot_eyetracker_data(subject):
         plt.close(fig)
 
         # Same but plot separately for the task relevance and durations separately:
-        for task_rel in task_relevance:
-            for dur in durations:
-                # Open a figure:
-                fig, ax = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True)
-                # Add the super title:
-                fig.suptitle("Task relevance: {}, Duration: {}".format(task_rel, dur))
-                for ind, lock in enumerate(locks):
+        for lock in locks:
+            # Open a figure:
+            fig, ax = plt.subplots(nrows=2, ncols=3, sharex=True, sharey=True, figsize=(20, 15))
+            for task_i, task_rel in enumerate(task_relevance):
+                for dur_i, dur in enumerate(durations):
+                    # Add the super title:
+                    fig.suptitle("Task relevance: {}, Duration: {}".format(task_rel, dur))
                     for soa in soas:
-                        data_l = pupil_epo.copy()["/".join([lock, soa, task_rel, dur])].get_data(picks=["LPupil"])
-                        data_r = pupil_epo.copy()["/".join([lock, soa, task_rel, dur])].get_data(picks=["RPupil"])
-                        avg_l = np.mean(np.squeeze(data_l), axis=0)
-                        avg_r = np.mean(np.squeeze(data_r), axis=0)
-                        err_l = sem(np.squeeze(data_l), axis=0)
-                        err_r = sem(np.squeeze(data_r), axis=0)
+                        data = np.mean(pupil_epo.copy()["/".join([lock, soa, task_rel,
+                                                                  dur])].get_data(picks=["LPupil",
+                                                                                         "RPupil"]), axis=1)
+                        avg = np.mean(np.squeeze(data), axis=0)
+                        err = sem(np.squeeze(data), axis=0)
                         # Plot the average pupil size in that condition:
-                        ax[ind, 0].plot(epochs.times, avg_l, label=soa)
+                        ax[task_i, dur_i].plot(epochs.times, avg, label=soa)
                         # Add the error around that:
-                        ax[ind, 0].fill_between(epochs.times, avg_l - err_l, avg_l + err_l,
-                                                alpha=0.3)
-                        # Plot the average pupil size in that condition:
-                        ax[ind, 1].plot(epochs.times, avg_r, label=soa)
-                        # Add the error around that:
-                        ax[ind, 1].fill_between(epochs.times, avg_r - err_r, avg_r + err_r,
-                                                alpha=0.3)
-                    ax[ind, 0].set_title("Lock: {}, Eye: {}".format(lock, "Left"))
-                    ax[ind, 1].set_title("Lock: {}, Eye: {}".format(lock, "Right"))
-                plt.legend()
-                plt.ylabel("Pupil size (norm.)")
-                plt.xlabel("Time (s)")
-                plt.tight_layout()
-                # Save the figure:
-                file_name = "sub-{}_ses-{}_task-{}_{}_desc-{}_ana-{}.png".format(sub, session, task, data_type,
-                                                                                 epoch_name, "pupil_size_{}_{}".format(task_rel, dur))
-                fig.savefig(Path(save_root, file_name))
-                plt.close(fig)
+                        ax[task_i, dur_i].fill_between(epochs.times, avg - err, avg + err,
+                                                       alpha=0.3)
+                        ax[task_i, dur_i].set_title("{}, {}".format(task_rel, dur))
+            plt.legend()
+            plt.ylabel("Pupil size (norm.)")
+            plt.xlabel("Time (s)")
+            plt.tight_layout()
+            # Save the figure:
+            file_name = "sub-{}_ses-{}_task-{}_{}_desc-{}_ana-{}.png".format(sub, session, task, data_type,
+                                                                             epoch_name,
+                                                                             "pupil_size_{}".format(lock))
+            fig.savefig(Path(save_root, file_name))
+            plt.close(fig)
 
 
 if __name__ == "__main__":
