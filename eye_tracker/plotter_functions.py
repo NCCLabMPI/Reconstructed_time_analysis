c7582dfa5e4cd1860023ee7e9e122559c5357d3b
Alex Lepauvre <alexander.lepauvre@ad.aesthetics.mpg.de>
2023-12-06 11:20:50 +0100

    Add function to plot within subject boxplot in a somewhat standardized fashion

--- eye_tracker/plotter_functions.py
+++ eye_tracker/plotter_functions.py
@@ -5,7 +5,6 @@ import matplotlib.patches as patches
 import numpy as np
 from general_helper_function import get_event_ts
 
-
 font = {'size': 18}
 matplotlib.rc('font', **font)
 # Get matplotlib colors:
@@ -119,3 +118,52 @@ def plot_events_latencies(epochs, lock, durations, soas, channels=None, audio_lo
         ax_boxplot.spines[['right', 'top']].set_visible(False)
 
     return fig
+
+
+def plot_within_subject_boxplot(data_df, within_column, between_column, dependent_variable, positions=None,
+                                ax=None, cousineau_correction=True, title="", xlabel="", ylabel=""):
+    """
+    This function generates within subject design boxplot with line plots connecting each subjects dots across
+    conditions. Further offers the option to apply Cousineau Morey correction. Importantly, data must be passed before
+    averaging. The within subject averaging occurs in the function directly!
+    :param data_df: (data frame) contains the data to plot
+    :param within_column: (string) name of the column containing the within subject labels. This should contain in
+    most cases the subject ID but in general whatever unit the measurement was repeated in
+    :param between_column: (string) name of the column containing the label of the experimental condition for which
+    we want to display the variance between.
+    :param dependent_variable: (string) name of the column containing the dependent variable to plot (reaction time...)
+    :param ax: (matplotlib ax object) ax on which to plot the image.
+    :param positions: (None or string or array-like) x axis position for the boxplot. If string,the value within the
+    data frame of that particular column are taken as position. The value in that column must be numerical. Handy if you
+    have a column encoding a continuous value that you want to plot along.
+    :param cousineau_correction: (bool) whether to apply cousineau Morey correction.
+    :param title: (string) title of the plot
+    :param xlabel: (string) xlabel
+    :param ylabel: (string) ylabel
+    :return:
+    """
+    if cousineau_correction:
+        data_df = cousineau_correction(data_df, within_column, between_column, dependent_variable)
+
+    # Average the data within subject and condition for the boxplot:
+    avg_data = data_df.groupby([within_column, between_column])[dependent_variable].mean().reset_index()
+    # Convert to 2D arrays for the line plot:
+    avg_data_2d = np.array([avg_data[avg_data[cond] == cond][dependent_variable].to_numpy()
+                            for cond in avg_data[between_column].unique()]).T
+
+    if ax is None:
+        fig, ax = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True, figsize=[14, 14])
+    if isinstance(positions, str):
+        positions = avg_data[between_column].unique()
+    elif positions is None:
+        positions = range(0, len(avg_data[between_column].unique()))
+    bplot = ax.boxplot(avg_data_2d, patch_artist=True, notch=True,
+                       positions=positions, widths=0.1)
+    lineplot = ax.plot(positions, avg_data_2d.T, linewidth=0.6,
+                       color=[0.5, 0.5, 0.5], alpha=0.5)
+    ax.tick_params(axis='x', labelrotation=45)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    return ax, bplot, lineplot
