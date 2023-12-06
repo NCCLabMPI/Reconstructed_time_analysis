80eddad9413c98dff8099568df7c2606d16a1641
Alex Lepauvre <alexander.lepauvre@ad.aesthetics.mpg.de>
2023-12-06 10:50:02 +0100

    Reorganize files and functions

--- eye_tracker/general_helper_function.py
+++ eye_tracker/general_helper_function.py
@@ -1,4 +1,54 @@
 from mne.baseline import rescale
+import numpy as np
+import pandas as pd
+
+
+def cousineau_morey_correction(data, within_col, between_col, dependent_var):
+    """
+    This scripts computes the Cousineau Morey correction of data in a dataframe. From each subject and condition,
+    the subject mean (across conditions) gets subtracted, and then the grand average gets added (
+    :param data:
+    :param within_col:
+    :param between_col:
+    :param dependent_var:
+    :return:
+    """
+    # Compute the subject-specific means:
+    subjects_mean = data.groupby([within_col])[dependent_var].mean().reset_index()
+    # Compute the grand mean:
+    grand_mean = np.mean(subjects_mean[dependent_var].to_numpy())
+    # Create a new DataFrame to store the corrected data
+    corrected_data = data.copy()
+
+    for subject in data[within_col].unique():
+        for between_cond in data[between_col].unique():
+            subject_mean = subjects_mean[subjects_mean[within_col] == subject][dependent_var].values
+            single_trial_data = data[(data[within_col] == subject) &
+                                     (data[between_col] == between_cond)][dependent_var].to_numpy()
+
+            # Apply the Cousineau-Morey correction and store it in the corrected_data DataFrame
+            idx = (data[within_col] == subject) & (data[between_col] == between_cond)
+            corrected_data.loc[idx, dependent_var] = (single_trial_data - subject_mean) + grand_mean
+
+    return corrected_data
+
+
+def get_event_ts(data, times):
+    """
+    This function returns the time stamp of a particular event. This converts time series of zeros and ones to
+    time stamps of the beginning of each event.
+    :param data: data containing the blinking information. The data should in the format of trials x time with 1 where
+    a subject was blinking and 0 otherwise
+    :param times: (array) the time vector
+    :return: a list of arrays containing the onset of the blinks for each trial
+    """
+    blinking_onsets = []
+    n_blinks_per_trial = []
+    data_onset = np.diff(data, axis=-1)
+    for trial in range(data.shape[0]):
+        blinking_onsets.append(times[np.where(data_onset[trial, :] == 1)[0]])
+        n_blinks_per_trial.append(len(blinking_onsets[-1]))
+    return blinking_onsets
 
 
 def baseline_scaling(epochs, correction_method="ratio", baseline=(None, 0), picks=None, n_jobs=1):
---
+++ eye_tracker/plotter_functions.py
@@ -0,0 +1,121 @@
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import matplotlib.patches as patches
+import numpy as np
+from general_helper_function import get_event_ts
+
+
+font = {'size': 18}
+matplotlib.rc('font', **font)
+# Get matplotlib colors:
+prop_cycle = plt.rcParams['axes.prop_cycle']
+cwheel = prop_cycle.by_key()['color']
+
+
+def plot_events_latencies(epochs, lock, durations, soas, channels=None, audio_lock=True):
+    """
+    This function generate events plots from mne epochs object containing eyetracking events data. It loops through
+    each durations and SOAs conditions (from our PRP experiment) and generates one subplot per duration. Each subplot is
+    actually 2 separate subplots. The first is a raster plot that depicts the time stamp of each event in a trial. The
+    second is a boxplot showing the latency of the response per soa.
+    :param epochs: (mne epochs object) contains the eyetracking event data. The events data are time series of zeros
+    and ones, ones marking when the event of interest occured (saccades, blinks, fixation).
+    :param lock: (string) whether the soa event were locked to the onset or to the offset of the stimuli.
+    :param durations: (list of strings) list of the duration condition.
+    :param soas: (list of strings) list of the soa codnitions
+    :param channels: (list of strings) name of the channels containing the event of interest. They come in pairs, right
+    and left eye.
+    :param audio_lock: (bool) the data were epochjed relative to the visual stimulus onset. But we can easily convert
+    to locked the auditory stimulus by cropping the data from the SOA duration onwards.
+    :return: fig: the plotted figure
+    """
+    if channels is None:
+        channels = ["Lblink", "Rblink"]
+
+    # Open a figure:
+    fig, ax = plt.subplots(figsize=[11, 9], sharex=True)
+    gs = GridSpec(len(durations) * 3, 1)
+
+    # Plot separately for the different durations:
+    for dur_ind, dur in enumerate(durations):
+        data = []
+        trials_soas = []
+        colors = []
+        for ind, soa in enumerate(soas):
+            # Extract the data:
+            if audio_lock:  # To have the data locked to the audio, crop at SOA
+                if lock == 'onset':  # If onset locked, crop from SOA to SOA + 1sec
+                    epochs_cropped = epochs.copy()["/".join([lock, dur, soa])].crop(float(soa),
+                                                                                    float(soa) + 1)
+                else:  # If offset locked, we need to add the trial duration on top
+                    epochs_cropped = epochs.copy()["/".join([lock, dur, soa])].crop(float(soa) +
+                                                                                    float(dur),
+                                                                                    float(soa) +
+                                                                                    float(dur) + 1)
+                data_l = np.squeeze(epochs_cropped.get_data(picks=channels[0]))
+                data_r = np.squeeze(epochs_cropped.get_data(picks=channels[1]))
+
+            else:
+                data_l = np.squeeze(epochs.copy()["/".join([lock, dur, soa])].get_data(picks=channels[0]))
+                data_r = np.squeeze(epochs.copy()["/".join([lock, dur, soa])].get_data(picks=channels[1]))
+
+            # Combine both eyes such that if we have a 1 in both arrays, then we have a 1, otherwise a 0:
+            data.append(np.logical_and(data_l, data_r).astype(int))
+            # Colors:
+            colors.append([cwheel[ind]] * data_l.shape[0])
+            # Append the trial SOAs:
+            trials_soas.extend([soa] * data_l.shape[0])
+        if audio_lock:
+            times = np.arange(0, 1 + 1 / epochs.info['sfreq'], 1 / epochs.info['sfreq'])
+        else:
+            times = epochs.times
+
+        # Extract the time stamp of each particular event:
+        onset_ts = get_event_ts(np.concatenate(data), times)
+
+        # Create the event plot:
+        ax_event = plt.subplot(gs[dur_ind * 3:dur_ind * 3 + 2])
+        ax_event.eventplot(onset_ts, colors=np.concatenate(colors), linelengths=8, linewidths=1.5,
+                           lineoffsets=np.arange(0, len(onset_ts)))
+        ax_event.set_ylim([0, len(onset_ts)])
+        ax_event.set_xlim([times[0], times[-1]])
+        plt.setp(ax_event.get_xticklabels(), visible=False)
+
+        # Create the boxplot underneath it:
+        # Reformat the data:
+        onset_ts = [[onset_ts[i] for i in list(np.where(np.array(trials_soas) == soa)[0])] for soa in soas]
+        onset_ts = [[item for sublist in l for item in sublist] for l in onset_ts]
+        # Create the boxplot:
+        ax_boxplot = plt.subplot(gs[dur_ind * 3 + 2])
+        bplot = ax_boxplot.boxplot(onset_ts, vert=False, patch_artist=True,  # fill with color
+                                   labels=soas, whis=[5, 95], showfliers=False)
+        # Set face colors:
+        for patch, color in zip(bplot['boxes'], cwheel[0:len(soas)]):
+            patch.set_facecolor(color)
+        ax_boxplot.set_xlim([times[0], times[-1]])
+
+        # Finally, if we are not plotting the data time locked the the audio stim, displaying the stimulus duration
+        # as a gray patch:
+        if not audio_lock:
+            rect = patches.Rectangle((0, 0), float(dur), ax_boxplot.get_ylim()[1],
+                                     linewidth=1, edgecolor='none', facecolor=[0.5, 0.5, 0.5], alpha=0.1,
+                                     zorder=1)
+            ax_boxplot.add_patch(rect)
+            rect = patches.Rectangle((0, 0), float(dur), ax_event.get_ylim()[1],
+                                     linewidth=1, edgecolor='none', facecolor=[0.5, 0.5, 0.5], alpha=0.1,
+                                     zorder=1)
+            ax_event.add_patch(rect)
+        # Axes deco:
+        if dur_ind == 0:
+            ax_event.set_title("Lock: {}".format(lock))
+        if dur_ind != len(durations) - 1:
+            plt.setp(ax_boxplot.get_xticklabels(), visible=False)
+        else:
+            ax_boxplot.set_ylabel('SOA')
+            ax_boxplot.set_xlabel('Time (sec)')
+            ax_event.set_ylabel('Trial')
+        ax_event.spines[['right', 'top']].set_visible(False)
+        ax_boxplot.spines[['right', 'top']].set_visible(False)
+
+    return fig
