8e51e79e2b5ffd7602ddb6e98e9dc0bb155fe31f
Alex Lepauvre <alexander.lepauvre@ad.aesthetics.mpg.de>
2023-12-08 20:10:30 +0100

    Add environment variable file
    To avoid having to set these in separate config files

---
+++ environment_variables.py
@@ -0,0 +1,13 @@
+
+bids_root = r"C:\Users\alexander.lepauvre\Documents\GitHub\Reconstructed_time_analysis\bids"
+
+colors = {
+    "soa": {
+        "0": [91/255, 192/255, 235/255],
+        "0.116": [244/255, 208/255, 11/255],
+        "0.232": [255/255, 136/255, 17/255],
+        "0.466": [8/255, 45/255, 15/255]
+    }
+}
+
+
