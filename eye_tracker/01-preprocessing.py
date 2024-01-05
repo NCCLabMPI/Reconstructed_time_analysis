6b560fcf294adf0f22110ec4a8bd8d1409dbce2b
Alex Lepauvre <alexander.lepauvre@ad.aesthetics.mpg.de>
2024-01-05 12:44:11 +0100

    Add all subjects to preprocessing

--- eye_tracker/01-preprocessing.py
+++ eye_tracker/01-preprocessing.py
@@ -206,7 +206,8 @@ if __name__ == "__main__":
     # SX101: differences in sampling rate due to experiment program issues
     # SX104: missing files
     # SX117: no eyetracking data
-    subjects_list = ["SX102"]
+    subjects_list = ["SX102", "SX103", "SX105", "SX106", "SX107", "SX108", "SX109", "SX110", "SX111", "SX112", "SX113",
+                     "SX114", "SX115", "SX116", "SX118", "SX119", "SX120", "SX121"]
     parameters_file = (
         r"C:\Users\alexander.lepauvre\Documents\GitHub\Reconstructed_time_analysis\eye_tracker"
         r"\01-preprocessing_parameters.json ")
