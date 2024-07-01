df131a1aa90a0b0914719f87471b42d9f2e50c66
Alex Lepauvre <alex.lepauvre@ae.mpg.de>
2024-07-01 09:43:36 +0200

    Update slurm_wrapper.py

--- slurm_wrapper.py
+++ slurm_wrapper.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+import mne
+import environment_variables as ev
 
 
 def decoding_batch(configs):
@@ -7,12 +9,17 @@ def decoding_batch(configs):
     This function runs jobs for each config found in the current config folder!
     :return:
     """
+    labels = mne.read_labels_from_annot("fsaverage", parc='aparc.a2009s', hemi='both', surf_name='pial',
+                                        subjects_dir=ev.fs_directory, sort=True)
+    roi_names = list(set(lbl.name.replace("-lh", "").replace("-rh", "")
+                         for lbl in labels if "unknown" not in lbl.name))
     # Launching a job for each:
     for config in configs:
-        # Run the rsa analysis script using the customized config file
-        run_command = f'sbatch SLURM_decoding.sh --config="{config}"'
-        print(run_command)
-        subprocess.Popen(run_command, shell=True)
+        for roi in roi_names:
+            # Run the rsa analysis script using the customized config file
+            run_command = f'sbatch SLURM_decoding.sh --config="{config}" --roi="{roi}"'
+            print(run_command)
+            subprocess.Popen(run_command, shell=True)
 
 
 if __name__ == "__main__":
